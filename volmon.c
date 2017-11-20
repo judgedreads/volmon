b243a8ffab539a0014cbbc97230e327838743e3a
judgedreads <pearcedavis92@gmail.com>
2017-11-19 17:00:27 -0800

    rewrite using mixer and callbacks
    - open mixer once and reuse the handle
    - use builtin callback functionality, rather than manual poll
    - get min and max volume to calculate percent
    - used amixer.c sevents function for reference

--- volmon.c
+++ volmon.c
@@ -1,101 +1,20 @@
-#include <stdio.h>
-#include <poll.h>
 #include <alsa/asoundlib.h>
-#include <alsa/control.h>
+#include <stdio.h>
 
-int subscribe(const char *card, snd_ctl_t **ctlp)
+long min, max;
+
+int print_vol(snd_mixer_elem_t *elem)
 {
-	int err;
-
-	err = snd_ctl_open(ctlp, card, SND_CTL_READONLY);
-	if (err < 0) {
-		fprintf(stderr, "Failed to open device: %s\n", card);
-		return err;
-	}
-	err = snd_ctl_subscribe_events(*ctlp, 1);
-	if (err < 0) {
-		fprintf(stderr, "Failed to subscribe to events on device: %s\n",
-			card);
-		snd_ctl_close(*ctlp);
-		return err;
-	}
+	long vol;
+	snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &vol);
+	printf("%ld%%\n", (vol * 100) / max);
 	return 0;
 }
 
-int print_vol(const char *card, const char *mix)
+int mixer_elem_cb(snd_mixer_elem_t *elem, unsigned int mask)
 {
-	int err = 0;
-	snd_hctl_t *hctl;
-	snd_ctl_elem_id_t *id;
-	snd_ctl_elem_value_t *control;
-
-	err = snd_hctl_open(&hctl, card, 0);
-	if (err != 0) {
-		fprintf(stderr, "Failed to open device: %s\n", card);
-		return err;
-	}
-	err = snd_hctl_load(hctl);
-	if (err != 0) {
-		fprintf(stderr, "Failed to load device: %s\n", card);
-		snd_hctl_close(hctl);
-		return err;
-	}
-
-	snd_ctl_elem_id_alloca(&id);
-	snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
-
-	int len_mix_name = strlen(mix) + 17;
-	char mix_name[len_mix_name];
-	sprintf(mix_name, "%s Playback Volume", mix);
-	snd_ctl_elem_id_set_name(id, mix_name);
-
-	snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
-	if (elem == NULL)
-		err = -1;
-
-	snd_ctl_elem_value_alloca(&control);
-	snd_ctl_elem_value_set_id(control, id);
-
-	err = snd_hctl_elem_read(elem, control);
-	if (err != 0) {
-		fprintf(stderr, "Failed to read volume\n");
-		snd_hctl_close(hctl);
-		return err;
-	}
-	int vol = (int) snd_ctl_elem_value_get_integer(control, 0);
-	if (vol >= 0) {
-		printf("%d\n", vol);
-		fflush(stdout);
-	} else {
-		fprintf(stderr, "Failed to read volume\n");
-		err = vol;
-	}
-	snd_hctl_close(hctl);
-	return err;
-}
-
-int handle_event(snd_ctl_t * ctl, const char *card, const char *mix)
-{
-	snd_ctl_event_t *event;
-	unsigned int mask;
-	int err;
-
-	snd_ctl_event_alloca(&event);
-	err = snd_ctl_read(ctl, event);
-	if (err < 0)
-		return err;
-
-	if (snd_ctl_event_get_type(event) != SND_CTL_EVENT_ELEM)
-		return 0;
-
-	mask = snd_ctl_event_elem_get_mask(event);
-	if (!(mask & SND_CTL_EVENT_MASK_VALUE))
-		return 0;
-
-	err = print_vol(card, mix);
-	if (err < 0)
-		return err;
-
+	if (mask & SND_CTL_EVENT_MASK_VALUE)
+		print_vol(elem);
 	return 0;
 }
 
@@ -115,41 +34,55 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		usage();
 
+	snd_mixer_t *handle;
+	snd_mixer_selem_id_t *sid;
 	const char *card = argv[1];
-	const char *mix = argv[2];
+	const char *selem_name = argv[2];
+	int err;
 
+	if ((err = snd_mixer_open(&handle, 0)) < 0) {
+		fprintf(stderr, "Failed to open mixer\n");
+		snd_mixer_close(handle);
+		return 1;
+	}
+	if ((err = snd_mixer_attach(handle, card)) < 0) {
+		fprintf(stderr, "Failed to attach %s\n", card);
+		snd_mixer_close(handle);
+		return 1;
+	}
+	if ((err = snd_mixer_selem_register(handle, NULL, NULL)) < 0) {
+		fprintf(stderr, "Failed to register mixer\n");
+		snd_mixer_close(handle);
+		return 1;
+	}
+	if ((err = snd_mixer_load(handle)) < 0) {
+		fprintf(stderr, "Failed to load mixer\n");
+		snd_mixer_close(handle);
+		return 1;
+	}
 
-	snd_ctl_t *ctl;
-	int err = 0;
+	snd_mixer_selem_id_alloca(&sid);
+	snd_mixer_selem_id_set_index(sid, 0);
+	snd_mixer_selem_id_set_name(sid, selem_name);
+	snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
 
-	err = print_vol(card, mix);
-	if (err < 0)
-		return err;
+	snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+	snd_mixer_elem_set_callback(elem, mixer_elem_cb);
 
-	err = subscribe(card, &ctl);
-	if (err < 0)
-		return err;
-
-	struct pollfd fd;
-	snd_ctl_poll_descriptors(ctl, &fd, 1);
+	print_vol(elem);
 	while (1) {
-		err = poll(&fd, 1, -1);
-		if (err <= 0) {
-			break;
-		}
-		unsigned short revents;
-		err = snd_ctl_poll_descriptors_revents(ctl, &fd, 1, &revents);
-		if (err != 0)
-			break;
-		if (!revents & POLLIN)
-			continue;
-		err = handle_event(ctl, card, mix);
-		if (err != 0) {
-			fprintf(stderr, "Failed to handle volume event\n");
-			break;
+		int res;
+		res = snd_mixer_wait(handle, -1);
+		if (res >= 0) {
+			res = snd_mixer_handle_events(handle);
+			if (res < 0) {
+				fprintf(stderr, "Failed to handle events\n");
+				snd_mixer_close(handle);
+				return 1;
+			}
 		}
 	}
 
-	snd_ctl_close(ctl);
-	return err;
+	snd_mixer_close(handle);
+	return 0;
 }
